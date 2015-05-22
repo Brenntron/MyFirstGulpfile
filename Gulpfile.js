069edea21fcb7866a7364fa58f645a0c4f4bfbc5
Brennan Willingham <brennanw22@gmail.com>
2015-05-22 11:48:39 -0500

    totally forgot to add the fulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp         = require('gulp'),
+    del          = require('del'),
+    sass         = require('gulp-sass'),
+    jade         = require('gulp-jade'),
+    autoprefixer = require('autoprefixer-core'),
+    postcss      = require('gulp-postcss');
+
+gulp.task('clean', function () {
+  del(['temp', 'public']);
+});
+
+gulp.task('sass', function () {
+  gulp
+    .src('app/styles/main.scss')
+    .pipe(sass())
+    .pipt(postcss([ autoprefixer({ browsers: ['last 2 browsers']}) ]))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('jade', function () {
+  gulp
+    .src('app/**/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('public'));
+});
